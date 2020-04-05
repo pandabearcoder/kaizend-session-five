983837983b0bbbf567aea7b8f556dbb9cd3ee7af
pandabearcoder <vlouie00@gmail.com>
2020-04-05 14:00:24 +0800

    Add files for requests lib

---
+++ requesting/topic.py
@@ -0,0 +1,21 @@
+import requests
+
+
+BASE_URL = 'http://sample-target-bucket-with-html-pages.s3-website-ap-southeast-1.amazonaws.com'
+
+
+def extract_html_content(target_url):
+    response = requests.get(target_url)
+    return response.text
+
+
+def main():
+    target_page = BASE_URL + '/index.html'
+    print(target_page)
+
+    html_content = extract_html_content(target_page)
+    print(html_content)
+
+
+if __name__ == '__main__':
+    main()
